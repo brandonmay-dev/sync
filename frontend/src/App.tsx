54e80d11938c30a19461022d6aec18fbcdfd46fa
Marqui <bmay86@protonmail.com>
2026-04-05 21:59:45 -0400

    Finish install Clerk and test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,20 @@
-import React from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { Show, SignInButton, SignUpButton, UserButton } from "@clerk/react";
 
 function App() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center gap-6 p-6">
-      <h1 className="text-red-600 text-5xl">Welcome to the App!</h1>
-
-      <Button>Click me</Button>
-
-      <div className="w-full max-w-sm">
-        <Input placeholder="Type something..." />
-      </div>
-    </div>
+    <>
+      <header>
+        <Show when="signed-out">
+          <SignInButton />
+          <SignUpButton />
+        </Show>
+        <Show when="signed-in">
+          <UserButton />
+        </Show>
+      </header>
+    </>
   );
 }
 
