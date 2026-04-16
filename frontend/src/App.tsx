11c991d277af4b92834200b01c4a63b2f3bdc057
Marqui <bmay86@protonmail.com>
2026-04-16 13:10:53 -0400

    feat: scaffold main layout and left sidebar navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,13 @@ import { Route, Routes } from "react-router-dom";
 import Homepage from "@/pages/home/HomePage";
 import AuthCallbackPage from "@/pages/auth-callback/AuthCallbackPage";
 import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
+import MainLayout from "./layout/MainLayout";
+import ChatPage from "@/pages/chat/ChatPage";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Homepage />} />
         <Route
           path="/sso-callback"
           element={
@@ -17,6 +18,11 @@ function App() {
           }
         />
         <Route path="/auth-callback" element={<AuthCallbackPage />} />
+
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/chat" element={<ChatPage />} />
+        </Route>
       </Routes>
     </>
   );
