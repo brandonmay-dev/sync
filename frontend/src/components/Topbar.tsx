700745b0981b7b7cf12e769e804628b787e8a17f
Marqui <bmay86@protonmail.com>
2026-04-19 03:53:55 -0400

    feat: implement home page UI

--- frontend/src/components/Topbar.tsx
+++ frontend/src/components/Topbar.tsx
@@ -1,10 +1,14 @@
-import { SignedIn, SignedOut, SignOutButton } from "@clerk/clerk-react";
+import { SignedOut, UserButton } from "@clerk/clerk-react";
 import { LayoutDashboardIcon } from "lucide-react";
 import { Link } from "react-router-dom";
 import SignInOAuthButtons from "./SignInOAuthButtons";
+import { useAuthStore } from "@/stores/useAuthStore";
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "./ui/button";
 
 const Topbar = () => {
-  const isAdmin = false;
+  const { isAdmin } = useAuthStore();
+  console.log({ isAdmin });
   return (
     <div
       className="flex items-center justify-between p-4 sticky top-0 bg-zinc-900/75 backdrop-blur-md z-10
@@ -13,17 +17,20 @@ const Topbar = () => {
       <div className="flex gap-2 items-center">Sync</div>
       <div className=" flex items-center gap-4">
         {isAdmin && (
-          <Link to={"/admin"}>
+          <Link
+            to={"/admin"}
+            className={cn(buttonVariants({ variant: "outline" }))}
+          >
             <LayoutDashboardIcon className="size-4 mr-2" />
+            Admin Dashboard
           </Link>
         )}
 
-        <SignedIn>
-          <SignOutButton />
-        </SignedIn>
         <SignedOut>
           <SignInOAuthButtons />
         </SignedOut>
+
+        <UserButton />
       </div>
     </div>
   );
---
+++ frontend/src/pages/home/components/SectionGrid.tsx
@@ -0,0 +1,51 @@
+import type { Song } from "@/types";
+import SectionGridSkeleton from "./SectionGridSkeleton";
+import { Button } from "@/components/ui/button";
+
+type SectionGridProps = {
+  title: string;
+  songs: Song[];
+  isLoading: boolean;
+};
+
+const SectionGrid = ({ title, songs, isLoading }: SectionGridProps) => {
+  if (isLoading) return <SectionGridSkeleton />;
+
+  return (
+    <div className="mb-8">
+      <div className="mb-4 flex items-center justify-between">
+        <h2 className="text-xl font-semibold sm:text-2xl">{title}</h2>
+        <Button
+          variant="link"
+          className="text-sm text-zinc-400 hover:text-white"
+        >
+          See all
+        </Button>
+      </div>
+
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4">
+        {songs.map((song) => (
+          <div
+            key={song._id}
+            className="group cursor-pointer rounded-md bg-zinc-800/40 p-4 transition-all hover:bg-zinc-700/40"
+          >
+            <div className="relative mb-4">
+              <div className="aspect-square overflow-hidden rounded-md shadow-lg">
+                <img
+                  src={song.imageUrl}
+                  alt={song.title}
+                  className="h-full w-full object-cover transition-transform duration-300 group-hover:scale-105"
+                />
+              </div>
+            </div>
+
+            <h3 className="truncate font-medium mb-2">{song.title}</h3>
+            <p className="text-sm text-zinc-400 truncate">{song.artist}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SectionGrid;
